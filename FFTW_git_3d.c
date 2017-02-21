a355287f70a7f845aa5a5bc54f42d3faad973ecf
root <root@mech-7.sr.unh.edu>
2017-02-20 23:08:06 -0500

    3D FOURN VS FFTW 3D Identical Results ! 2/20/2017

---
+++ FFTW_git_3d.c
@@ -0,0 +1,46 @@
+#include <fftw3.h>
+#include<time.h>
+#include <sys/time.h> 
+
+#include <stdio.h>
+#include <math.h>
+
+#include "fftw_test.c"
+
+
+int main()
+{
+  static double acc_time;
+  static int acc_n;
+  
+  
+  static fftw_complex signal[NUM_POINTS];
+  static fftw_complex result[NUM_POINTS];
+  
+  fftw_plan plan = fftw_plan_dft_3d(NUM_POINTS1,
+				    NUM_POINTS2,
+				    NUM_POINTS3,
+				    signal,
+				    result,
+				    FFTW_FORWARD,
+				    FFTW_ESTIMATE);
+  
+  acquire_from_somewhere(signal);
+  //do_something_with(signal);
+
+  struct timespec now, tmstart;
+  clock_gettime(CLOCK_REALTIME, &tmstart);
+  for (int i = 0; i < 10; i++) {
+    fftw_execute(plan);
+  }
+  clock_gettime(CLOCK_REALTIME, &now);
+  acc_time += (now.tv_sec+now.tv_nsec*1e-9) - (tmstart.tv_sec+tmstart.tv_nsec*1e-9);
+  acc_n++;
+
+  printf("avg FFTW  avg time: %g total time %g\n", acc_time / acc_n, acc_time);
+    
+    do_something_with(result);
+  
+  fftw_destroy_plan(plan);
+  return 0;
+}
--- Makefile
+++ Makefile
@@ -4,16 +4,30 @@ CFLAGS = -tp=p7 -fast
 
 LOADLIBES = -lfftw3 -lrt
 
-all: FFTW_git FFT_fourn
 
-FFT_fourn: FFT_fourn.o fourn.o
+    #### FOR 1D #####
 
-FFT_fourn.o: FFT_fourn.c fftw_test.c
+#all: FFTW_git FFT_fourn
 
-FFTW_git.o: FFTW_git.c fftw_test.c
+#FFT_fourn: FFT_fourn.o fourn.o
+
+#FFT_fourn.o: FFT_fourn.c fftw_test.c
+
+#FFTW_git.o: FFTW_git.c fftw_test.c
+
+#clean:
+#	rm -f FFTW_git FFT_fourn *.o
+
+   
+    #### FOR 3D #####
+
+all: FFTW_git_3d FFT_fourn_3d
+
+FFT_fourn_3d: FFT_fourn_3d.o fourn.o
+
+FFT_fourn_3d.o: FFT_fourn_3d.c fftw_test.c
+
+FFTW_git_3d.o: FFTW_git_3d.c fftw_test.c
 
 clean:
-	rm -f FFTW_git FFT_fourn *.o
-
-
-
+	rm -f FFTW_git_3d FFT_fourn_3d *.o
